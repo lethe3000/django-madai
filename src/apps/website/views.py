cadc045b82501d52ae8e27b4bd505321f70258f5
sanford <707959992@qq.com>
2014-05-19 23:29:45 +0800

    retrieve travel notes

--- src/apps/website/views.py
+++ src/apps/website/views.py
@@ -5,6 +5,7 @@ import os
 from django.template.response import TemplateResponse
 from apps.hotel.models import Hotel
 from apps.present.models import Present
+from apps.share.models import TravelNote
 
 logger = logging.getLogger('apps.' + os.path.basename(os.path.dirname(__file__)))
 
@@ -13,7 +14,7 @@ def index(request):
     hotels = Hotel.active_objects.order_by('display_order').filter(is_pinned=True, is_published=True)[0: 9]
     hotel_banners = Hotel.active_objects.order_by('display_order').filter(is_banner=True, is_published=True)[0: 3]
     presents = Present.active_objects.order_by('display_order').filter(is_pinned=True, is_published=True)[0:9]
-    # present_banners = Present.active_objects.order_by('display_order').filter(is_banner=True, is_published=True)[0:3]
+    travel_notes = TravelNote.active_objects.order_by('display_order').filter(is_pinned=True, is_published=True)[0: 9]
     return TemplateResponse(request, 'website/index.html', locals())
 
 
